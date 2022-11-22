2c2f9e6dc8135df13d1dee7caf6787dc11191bd5
stream1080 <fmxhax638@foxmail.com>
2022-11-22 11:40:45 +0800

    feat: 修改 mybatis-plus 配置

---
+++ src/main/java/com/example/demo/config/MybatisPlusConfig.java
@@ -0,0 +1,40 @@
+package com.example.demo.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * MybatisPlus 配置
+ *
+ * @author stream
+ */
+@Configuration
+@MapperScan("com.example.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     * 一缓和二缓遵循mybatis的规则
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+
+        // 分页插件: PaginationInnerInterceptor
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor(DbType.MYSQL);
+        paginationInnerInterceptor.setMaxLimit(1000L);
+        interceptor.addInnerInterceptor(paginationInnerInterceptor);
+
+        // 防止全表更新与删除插件: BlockAttackInnerInterceptor
+        BlockAttackInnerInterceptor blockAttackInnerInterceptor = new BlockAttackInnerInterceptor();
+        interceptor.addInnerInterceptor(blockAttackInnerInterceptor);
+
+        return interceptor;
+    }
+}
